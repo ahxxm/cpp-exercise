fdefc450f31b064e7ffc67db672706a0a12e696e
ahxxm <i@ahxxm.com>
2015-12-20 22:28:48 +0800

    exercise 30 and 31 of chapter 2

---
+++ src/2/30-31-top-low-level-const.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void top_low_level_const(){
+  // Definitions given by the book
+  int i = 0;
+  const int v2 = 1;
+  int v1 = v2;
+  int *p1 = &v1, &r1 = v1;
+  const int *p2 = &v2, *const p3 = &i, &r2 = v2;
+
+  // legal tests
+
+  // yes, r1 is a reference.
+  r1 = v2;
+
+  // p1 = p2;
+  // no, p1 does not point to const int.
+
+  // yes, previously p2 points to v2, now it points to v1.
+  p2 = p1;
+
+  // p1 = p3;
+  // no, p3 is "const int *const".
+
+  // yes, it points to converted const value, then converted i(what p3 points to).
+  p2 = p3;
+}
