e04de7f375ba6db0d7b81d2d42b838e775229099
ahxxm <i@ahxxm.com>
2015-12-21 17:43:23 +0800

    add exercise 35

---
+++ src/2/35-determine-auto-types.cpp
@@ -0,0 +1,15 @@
+
+void determine_type(){
+
+  // const int
+  const int i = 42;
+
+  //  int
+  auto j = i;
+
+  //  const int reference
+  const auto &k = i;
+
+  // const int*
+  auto *p = &i;
+}
