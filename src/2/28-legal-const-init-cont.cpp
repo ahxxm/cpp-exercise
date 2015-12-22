c9022ebebd6d800b9aed7a18bd04c741ba1cb546
ahxxm <i@ahxxm.com>
2015-12-22 11:36:42 +0800

    resolve a fixme

--- src/2/28-legal-const-init-cont.cpp
+++ src/2/28-legal-const-init-cont.cpp
@@ -2,7 +2,9 @@
 
 void const_init_cont(){
   // int i, *const cp = 1.233;
-  // FIXME: no, cp is not initialized. why it's treated as int??
+  // no, cp is a const pointer to int, but 1.233 is not int.
+  // to make this happen, define a int then point to it:
+  // int jj = 233;int i,* const cp = &jj;
 
   // const int ic,& r = ic;
   // no, ic is not initialized.
@@ -11,9 +13,9 @@ void const_init_cont(){
   // need initialize too.
 
   // p itself can change to point to other const objects.
-  int i = 5;
+  int iv = 5;
   int j = 6;
   const int *p;
-  p = &i;
+  p = &iv;
   p = &j;
 }
