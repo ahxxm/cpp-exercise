11901156b66d0426a3c046adfa9202631eccc551
ahxxm <i@ahxxm.com>
2015-12-24 21:53:33 +0800

    exercise 18 19 20 of chap 3

---
+++ src/3/18-19-20-vector-basic-ops.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+void basic_vector_ops(int argc, char *argv[]){
+  // 18: index/subscription does not change a vector, use push_back
+  // instead
+  std::vector<std::string> miu;
+  miu.push_back("something");
+
+  // 19: initialize with 10 * "42"
+  std::vector<std::string> v1(10, "42");
+  std::vector<std::string> v2; // manually..
+  for (int i = 0; i < 10; i++) {
+    v2.push_back("42");
+  }
+
+  // 20: vector<int> print
+  // FIXME: wat{1,2,3,4} does not work?
+  std::vector<int> wat;
+  wat.push_back(1);
+  wat.push_back(2);
+  wat.push_back(4);
+
+  // sum of adjacent ones: loop till last one
+  int j = wat.size() - 1;
+  for (int i = 0; i < j; i++) {
+    std::cout << wat[i] + wat[i + 1] << std::endl;
+  }
+
+  // first and last element
+  std::cout << wat[0] << "wat" << wat[j] << std::endl;
+}
