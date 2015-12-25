564f720b56ab42b13ce1fcbff9deb7d641610d6a
ahxxm <i@ahxxm.com>
2015-12-25 21:48:13 +0800

    first arrow operator exercise

---
+++ src/3/intro-arrow-operator.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+void double_a_vector(int argc, char *argv[]){
+
+  // create a vector of length 10
+  // FIXME: any elegant solution?
+  std::vector<int> ten;
+  for (int i = 0; i != 10; i++) {
+    ten.push_back(i);
+  };
+
+  // double each element, print this vector
+  for (auto j = ten.begin(); j != ten.end(); ++j) {
+    *j *= 2;
+    std::cout <<* j << std::endl;
+  }
+}
