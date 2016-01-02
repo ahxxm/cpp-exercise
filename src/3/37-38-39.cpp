39ddf31570b42f7cc4c008404e8678f674e5a9d8
ahxxm <i@ahxxm.com>
2016-01-02 18:56:58 +0800

    exercise

---
+++ src/3/37-38-39.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+void exercise_37(int argc, char *argv[]){
+
+  // 37: this snippet prints y a y line by line
+  // const char ca[] = {'y', 'a', 'y'};
+  // const char *cp = ca;
+  // while (*cp) {
+  // std::cout << *cp << std::endl;
+  // ++cp;
+  // };
+
+  // FIXME: why these will cause the first string definition
+  // to cout?
+
+  // 38: why adding 2 pointers meaningless?
+  // because pointers are address?
+
+  // 39: compare 2 string
+  // FIXME: any special comparison technique?
+  std::string a = "wuto";
+  std::string b = "wut";
+
+  std::string result = "equal";
+  if (a < b) {
+    result = "less";
+  } else {
+    if (a > b) {
+      result = "greater";
+    }
+  }
+
+  std::cout << result << std::endl;
+}
