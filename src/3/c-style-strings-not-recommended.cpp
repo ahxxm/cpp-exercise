404e5faf038163b8bdc736275860657bd049295b
ahxxm <i@ahxxm.com>
2016-01-02 17:49:19 +0800

    comment out

--- src/3/c-style-strings-not-recommended.cpp
+++ src/3/c-style-strings-not-recommended.cpp
@@ -5,10 +5,10 @@ void c_style_string(int argc, char *argv[]){
 
   // string functions C-style: must pass null-ended arrays
   // not counting the tail null
-  std::cout << strlen("123") << std::endl;
+  // std::cout << strlen("123") << std::endl;
 
   // 0 for equality, positive if str1>str2, vice versa
-  std::cout << strcmp("123", "123") << std::endl;
+  // std::cout << strcmp("123", "123") << std::endl;
 
   // c++11 does not allow string literal to be converted
   // to char* ...
