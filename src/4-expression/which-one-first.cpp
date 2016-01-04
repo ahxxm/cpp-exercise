b2abb905b51d3ad448b68b131ebe4cea4cb4b84b
ahxxm <i@ahxxm.com>
2016-01-04 23:15:09 +0800

    2 rule
    parenthesize expressions to force grouping
    
    simplify compound expression

---
+++ src/4-expression/which-one-first.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+std::string a() {
+  std::cout << "a" << std::endl;
+  return "1";
+}
+
+std::string b() {
+  std::cout << "b" << std::endl;
+  return "2";
+}
+
+std::string c() {
+  std::cout << "c" << std::endl;
+  return "c";
+}
+
+int order(int argc, char *argv[]) {
+
+  // there is no guarantee which one execute first
+  std::string result = b() + (a() + c()) + b();
+  std::cout << result << std::endl;
+
+  return 0;
+}
