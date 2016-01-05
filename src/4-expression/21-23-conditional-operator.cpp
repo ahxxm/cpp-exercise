7a7a696ee377d435f9db0e244c216eb42ea11ca7
ahxxm <i@ahxxm.com>
2016-01-05 14:57:20 +0800

    ?: operator

---
+++ src/4-expression/21-23-conditional-operator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool is_odd(int &j) {
+
+  if (int((j - 1) / 2) == int(j / 2)) {
+    return true;
+  }
+
+  return false;
+}
+void conditional_operator() {
+  // FIXME: how to assign j to i and *p inline
+  int i, *p = 0;
+  int j = 2;
+  std::cout << "jar" << std::endl;
+
+  // member access operator ->
+  std::string s1 = "string";
+  std::string *pp = &s1;
+  long nn = pp->size();
+  std::cout << nn << std::endl;
+
+  // 21: double odd value in vector
+  // using ?: operator...
+  std::cout << "Double odd value" << std::endl;
+  std::vector<int> va {1, 2, 3, 4, 5, 6, 7};
+  for (int &i: va) {
+    if (is_odd(i)) {
+      // i *= 2;
+      std::cout << "Doubled: " << i * 2 << std::endl;
+    };
+
+    // ?: usage
+    std::cout << i << " " << ((int((i - 1) /  2) !=  int(i / 2)) ? "is even" : "is odd") << std::endl;
+
+  }
+
+  // 23: fix error caused by operator precedence
+  std::string s = "word";
+  std::string pl = s + (s[s.size() - 1] == 's' ? "" : "s");
+  std::cout << pl << std::endl;
+}
