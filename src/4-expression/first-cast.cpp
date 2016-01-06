453f771bb935f74e470a7306ab0ca06d34f6d850
ahxxm <i@ahxxm.com>
2016-01-06 13:48:05 +0800

    type cast

---
+++ src/4-expression/first-cast.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void first_cast(int argc, char *argv[]) {
+  double d = 3.14;
+
+  // couvert back to double pointer will preserve its values
+  void *p = &d;
+  std::cout << p << std::endl;
+
+  double *dp = static_cast<double*>(p);
+  std::cout << *dp << std::endl;
+
+  // const_cast
+  // ok, but writing *pc is undefined.
+  // FIXME: "can be useful in ocntext of overloaded functions, other uses often indicate design flaw."
+  char u = 'u';
+  const char *pc = &u;
+  char *pcc = const_cast<char*>(pc);
+  std::cout << *pcc << std::endl;
+}
