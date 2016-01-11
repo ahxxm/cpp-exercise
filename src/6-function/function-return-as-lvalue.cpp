0db8c1d3b4bf365221b5b1e6a01438f20becd3b1
ahxxm <i@ahxxm.com>
2016-01-11 14:18:26 +0800

    whether return value could be lvalue depends on return type

---
+++ src/6-function/function-return-as-lvalue.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+// This return value could be lvalue
+char &get_val_in_string(std::string &str, std::string::size_type ix) {
+  return str[ix];
+}
+
+int lower_a_string(int argc, char *argv[]) {
+  std::string s = "Hello";
+  get_val_in_string(s, 0) = 'h';
+  std::cout << s << std::endl;
+
+  return 0;
+}
