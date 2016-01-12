a83a01f4df76e08407c3a9a793c0acd9fa85dc30
ahxxm <i@ahxxm.com>
2016-01-12 19:02:30 +0800

    function overload

---
+++ src/6-function/39-overload.cpp
@@ -0,0 +1,26 @@
+// top level const equals to redeclare
+int lookup(char);
+int lookup(const char);
+
+// const to reference or pointer are low level
+// this way lookup is overloaded: compiler can
+// use constness of argument to distinguish
+int lookup(char&);
+int lookup(const char&);
+
+int lookup(char *);
+int lookup(const char *);
+
+// 39: explain each second declaration
+// a): overload
+int calc(int, int);
+int calc(const int, const int);
+
+// b): error, functions that differ only by return type
+// can't be overloaded
+int get();
+// double get();
+
+// c): overload..
+int *reset(int *);
+double *reset(double *);
