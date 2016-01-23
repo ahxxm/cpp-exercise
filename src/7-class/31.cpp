42dedb1024198e3aa75ffd69946a606e4b2ba649
ahxxm <i@ahxxm.com>
2016-01-23 14:02:45 +0800

    exercise 31

---
+++ src/7-class/31.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+
+// 31: class X and Y
+// X has pointer to Y
+// Y has object of type X
+
+// forward declaration
+class Y;
+
+class X {
+public:
+  Y *pointer;
+};
+
+class Y {
+public:
+  X object;
+};
+
+
+
+TEST(FirstClassExerciseTest, SomeTest) {
+  X testX;
+  std::cout << &(testX.pointer->object) << std::endl;
+
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
