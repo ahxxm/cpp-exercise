e9ee5010eeb74ec2374cb049a4d2c90468c384b7
ahxxm <i@ahxxm.com>
2016-01-30 22:23:55 +0800

    exercise 4

--- src/8-io/filestream.cpp
+++ src/8-io/filestream.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <vector>
 
 // fstream: file stream
 // - ifstream: read file
@@ -29,8 +30,33 @@ void open(const std::string &filename) {
 };
 
 
+// 8.4: read list of files' contents
+// into vector
+// http://stackoverflow.com/questions/195323/what-is-the-most-elegant-way-to-read-a-text-file-with-c
+std::vector<std::string> read_files(std::vector<std::string> filenames) {
+  std::vector<std::string> contents;
+
+  for (unsigned long i = 0; i != filenames.size(); ++i) {
+    std::string filename = filenames[i];
+    std::stringstream buffer;
+    std::ifstream input(filename);
+
+    std::cout << filename << std::endl;
+    buffer << input.rdbuf();
+    contents.push_back(buffer.str());
+  }
+
+  return contents;
+};
+
+
 TEST(MoreClassExerciseTest, SomeTest) {
   open("test.txt");
+
+  std::vector<std::string> filenames;
+  std::vector<std::string> contents;
+  filenames.push_back("test.txt");
+  contents = read_files(filenames);
   EXPECT_EQ(1, 1);
 }
 
