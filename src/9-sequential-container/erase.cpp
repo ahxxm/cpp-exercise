524fd86adeafcfd359c4be176454c62d1130c40a
ahxxm <i@ahxxm.com>
2016-02-04 20:46:41 +0800

    list erase test

---
+++ src/9-sequential-container/erase.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include <list>
+
+
+TEST(ListEraseTest, SomeTest) {
+  std::list<int> lst = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+  auto it = lst.begin();
+
+  while (it != lst.end()) {
+    if ( * it % 2) {
+      int previous_val = *it;
+      it = lst.erase(it);
+      int new_val = *it;
+      EXPECT_EQ(previous_val + 1, new_val);
+    } else {
+      ++it;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
