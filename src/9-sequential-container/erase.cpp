18db265fdc887269ccf9f41d976da77fc2c7b3ba
ahxxm <i@ahxxm.com>
2016-02-04 21:35:50 +0800

    exercise 25 26

--- src/9-sequential-container/erase.cpp
+++ src/9-sequential-container/erase.cpp
@@ -1,6 +1,26 @@
 #include "gtest/gtest.h"
 #include <list>
 
+int erase_test() {
+  std::list<int> lst1 = {0, 1, 2, 3, 4, 5, 6};
+
+  // what if elem1 and elem2 are equal
+  // what if they are both .end
+  // A: nothing happends..
+  auto elem1 = lst1.end();
+  auto elem2 = lst1.end();
+  lst1.erase(elem1, elem2);
+
+  auto length = lst1.size();
+  return length;
+}
+
+std::list<int> copy_array_into_list() {
+  int ia[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 55, 89};
+  std::list<int> va = {std::begin(ia), std::end(ia)};
+  return va;
+}
+
 
 TEST(ListEraseTest, SomeTest) {
   std::list<int> lst = {0, 1, 2, 3, 4, 5, 6, 7, 8};
@@ -11,6 +31,8 @@ TEST(ListEraseTest, SomeTest) {
       int previous_val = *it;
       it = lst.erase(it);
       int new_val = *it;
+      // returns iterator to
+      // the element just after the last removed one
       EXPECT_EQ(previous_val + 1, new_val);
     } else {
       ++it;
@@ -20,6 +42,17 @@ TEST(ListEraseTest, SomeTest) {
   // removed from lst
   auto after_len = lst.size();
   EXPECT_EQ(after_len, 5);
+
+  // 9.25 erase test
+  int length = erase_test();
+  EXPECT_EQ(length, 7);
+
+  // 9.26 array to list
+  auto converted_lst = copy_array_into_list();
+  EXPECT_EQ(converted_lst.size(), 11);
+  // single iterator form of erase remove odd values
+  // check `while` part above..
+
 }
 
 int main(int argc, char *argv[]) {
