dc5da4a303442e8b4822cd3a6f165d7f9ae722e8
ahxxm <i@ahxxm.com>
2016-02-05 20:37:46 +0800

    exercise 44

--- src/9-sequential-container/43-44.cpp
+++ src/9-sequential-container/43-44.cpp
@@ -21,6 +21,24 @@ void erase_ins(std::string &st, const std::string &old, const std::string &newVa
   }
 }
 
+// 44: rewrite with replace
+void rep(std::string &st, const std::string &old, const std::string &newVal) {
+  auto old_size = old.size();
+
+  for (unsigned long i = 0; i <= st.size() - old_size; ++i) {
+    std::string cur_string = std::string(st.begin() + i, st.begin() + i + old_size);
+
+    if (cur_string == old) {
+      // i: start index
+      // old_size: length of substr to be replaced
+      // finally: increment i to character one pass last
+      // element of newVal
+      st.replace(i, old_size, newVal);
+      i += old_size;
+    }
+  }
+}
+
 
 TEST(StringReplaceTest, SomeTest) {
   std::string st = "thintho";
@@ -29,6 +47,10 @@ TEST(StringReplaceTest, SomeTest) {
 
   erase_ins(st, old, newVal);
   EXPECT_EQ(st, "tinto");
+
+  std::string st2 = "thintho";
+  rep(st2, old, newVal);
+  EXPECT_EQ(st2, "tinto");
 }
 
 int main(int argc, char *argv[]) {
