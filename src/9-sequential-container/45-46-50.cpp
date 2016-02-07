42d45cbba094085068789f327c744757c6caac73
ahxxm <i@ahxxm.com>
2016-02-07 08:56:48 +0800

    exercise 50

--- src/9-sequential-container/45-46-50.cpp
+++ src/9-sequential-container/45-46-50.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <vector>
 
 // 45: iterator, insert and append
 // add prefix and suffix to a string
@@ -13,6 +14,16 @@ void insert_prefix_suffix(std::string &s, const std::string &prefix, const std::
   s.insert(s.end(), suffix.begin(), suffix.end());
 }
 
+// 50:
+int sum_vector_int_string(std::vector<std::string> &va) {
+  int result = 0;
+  for (auto i: va) {
+    result += std::stoi(i);
+  }
+
+  return result;
+}
+
 
 TEST(MoreClassExerciseTest, SomeTest) {
   std::string st = "string";
@@ -24,6 +35,10 @@ TEST(MoreClassExerciseTest, SomeTest) {
   std::string st2 = "st";
   insert_prefix_suffix(st2, pre, suf);
   EXPECT_EQ(st2, "pstd");
+
+  std::vector<std::string> vi = {"1234", "123a"};
+  int sum = sum_vector_int_string(vi);
+  EXPECT_EQ(sum, 1357);
 
 }
 
