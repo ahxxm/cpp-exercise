31f09100baabed7694abb3d083d5fbfc16d4acda
ahxxm <i@ahxxm.com>
2016-02-10 14:40:17 +0800

    exercise 20 21

---
+++ src/10-generic-algo/20-21.cpp
@@ -0,0 +1,37 @@
+#include "gtest/gtest.h"
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+
+// 20
+int count_if_lambda() {
+  std::vector<int> ve = {1, 2, 3, 4, 1, 2, 3};
+  int count_one = std::count_if(ve.begin(), ve.end(), [](int i)->int {return i == 1;});
+  std::cout << count_one << std::endl;
+  return count_one;
+}
+
+// 21
+int decrease_lambda() {
+  int i = 10;
+  auto f = [&i]() {
+    while (i != 0) {
+      --i;
+    }
+  };
+  f();
+  return i;
+}
+
+
+TEST(LambdaeTest, MoreTest) {
+  EXPECT_EQ(count_if_lambda(), 2);
+  EXPECT_EQ(decrease_lambda(), 0);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
