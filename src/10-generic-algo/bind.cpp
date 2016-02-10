7d970565db155d714de542d7e2c7c8f160b26a51
ahxxm <i@ahxxm.com>
2016-02-10 14:57:59 +0800

    ref in bind

--- src/10-generic-algo/bind.cpp
+++ src/10-generic-algo/bind.cpp
@@ -10,6 +10,16 @@ TEST(BindTest, SomeTest) {
   auto newIsLonger = std::bind(isLonger, std::placeholders::_1, 2);
   std::string st = "12345";
   EXPECT_EQ(newIsLonger(st), true);
+
+  // Bind can re-arrange param
+  auto rearrangedLongerThanTwo = std::bind(isLonger, std::placeholders::_2, std::placeholders::_1);
+  EXPECT_EQ(rearrangedLongerThanTwo(2, st), true);
+
+  // bind copies value,
+  // cref()/ref() makes it reference
+  // _2 means param's origin position in isLonger was 2
+  auto refBindLonger = std::bind(isLonger, std::ref(st), std::placeholders::_2);
+  EXPECT_EQ(refBindLonger(st, 4), true);
 }
 
 int main(int argc, char *argv[]) {
