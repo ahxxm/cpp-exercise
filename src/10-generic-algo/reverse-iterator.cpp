06eaedaf9867e3c9fc8c9be3f297488345d2f4c8
ahxxm <i@ahxxm.com>
2016-02-11 13:23:02 +0800

    exercise 36

--- src/10-generic-algo/reverse-iterator.cpp
+++ src/10-generic-algo/reverse-iterator.cpp
@@ -1,7 +1,8 @@
 #include "gtest/gtest.h"
-#include <vector>
+#include <list>
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 // 34
 void print_vector_reversely(const std::vector<std::string> &sa) {
@@ -22,6 +23,13 @@ TEST(ReverseIteratorTest, SomeTest) {
   std::vector<std::string> sa = {"123", "456", "789"};
   print_vector_reversely(sa);
   print_vector_reversely_using_ordinary_iterator(sa);
+}
+
+TEST(FindTest, SomeFindTest) {
+  // 36:
+  std::list<int> lst = {1, 2, 3, 4, 0, 5};
+  auto result = std::find(lst.crbegin(), lst.crend(), 0);
+  EXPECT_EQ(result.base() == lst.cend(), false);
 }
 
 int main(int argc, char *argv[]) {
