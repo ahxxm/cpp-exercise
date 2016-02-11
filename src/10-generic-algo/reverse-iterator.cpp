8ccbc60c0cc4c942ee3f45dfc69e4791787d6c5b
ahxxm <i@ahxxm.com>
2016-02-11 13:31:32 +0800

    exercise 37

--- src/10-generic-algo/reverse-iterator.cpp
+++ src/10-generic-algo/reverse-iterator.cpp
@@ -32,6 +32,31 @@ TEST(FindTest, SomeFindTest) {
   EXPECT_EQ(result.base() == lst.cend(), false);
 }
 
+std::list<int> copy_from_vector(const std::vector<int> &ve) {
+  std::list<int> lst;
+  if (ve.size() < 10) {
+    return lst;
+  }
+
+  std::list<int> sub_ve(ve.begin() + 2, ve.begin() + 7);
+  return sub_ve;
+}
+
+// 37: copy 3-7 reversely into a list
+TEST(VectorReverseCopyTest, SomeVectorTest) {
+  std::vector<int> ve = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  std::vector<int> ve2 = {1, 2, 3};
+  std::list<int> result = {3, 4, 5, 6, 7};
+  std::list<int> blank_result;
+
+  auto aa = copy_from_vector(ve);
+  EXPECT_EQ(aa, result);
+
+  auto bb = copy_from_vector(ve2);
+  EXPECT_EQ(bb, blank_result);
+
+}
+
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   int ret = RUN_ALL_TESTS();
