02bf6e6ac764f8a0b2c499e9c6feb2dadc846cbe
ahxxm <i@ahxxm.com>
2016-02-13 22:47:03 +0800

    std::hash

---
+++ src/11-associative-containers/hash.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <string>
+
+
+TEST(HashTest, SomeTest) {
+  auto result = std::hash<std::string>()("java");
+  auto result2 = std::hash<std::string>()("java");
+  std::cout << result << std::endl;
+  std::cout << result2 << std::endl;
+  EXPECT_EQ(result, result2);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
