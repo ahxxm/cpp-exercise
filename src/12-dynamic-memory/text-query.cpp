8aa800a6363d149cf5fb55909b14f4f41f90e7f4
ahxxm <i@ahxxm.com>
2016-02-19 17:22:19 +0800

    implement query and result

--- src/12-dynamic-memory/text-query.cpp
+++ src/12-dynamic-memory/text-query.cpp
@@ -6,18 +6,16 @@
 #include <map>
 #include <set>
 
-
+using line_no = std::vector<std::string>::size_type;
 class QueryResult;
 
 class TextQuery {
 public:
-  using line_no = std::vector<std::string>::size_type;
-
   TextQuery() = default;
   TextQuery(std::ifstream &infile): file(new std::vector<std::string>){
     std::string text;
-    // FIXME: getline no matching function
     while (std::getline(infile, text)) {
+      // FIXME: getline no matching function
       // read file into vector line by line
       file->push_back(text);
       int cur_line = file->size() - 1;
@@ -36,8 +34,8 @@ public:
     }
   };
 
-  // FIXME:
   QueryResult query(const std::string &) const;
+
 private:
   // input file
   std::shared_ptr<std::vector<std::string>> file;
@@ -45,6 +43,45 @@ private:
   // word->(line1,line2)
   std::map<std::string, std::shared_ptr<std::set<line_no>>> word_line_map;
 };
+
+
+class QueryResult {
+  friend std::ostream &print(std::ostream &os, const QueryResult &qr) {
+    // print count and each line
+    os << qr.sought << " occurs " << qr.lines->size() << " time(s)." << std::endl;
+    for (line_no num: *qr.lines) {
+      os << "\t(line " << num + 1 << ") "
+         << *(qr.file->begin() + num) << std::endl;
+    }
+    return os;
+  }
+
+public:
+  QueryResult(std::string s,
+              std::shared_ptr<std::set<line_no>> p,
+              std::shared_ptr<std::vector<std::string>> f):
+    sought(s), lines(p), file(f) {};
+
+private:
+  std::string sought;
+  std::shared_ptr<std::set<line_no>> lines;
+  std::shared_ptr<std::vector<std::string>> file;
+
+};
+
+QueryResult TextQuery::query(const std::string &keyword) const {
+  // if no data if found..
+  static std::shared_ptr<std::set<line_no>> nodata(new std::set<line_no>);
+
+  auto loc = word_line_map.find(keyword);
+  if (loc == word_line_map.end()) {
+    return QueryResult(keyword, nodata, file);
+  } else {
+    // second is line_no set
+    return QueryResult(keyword, loc->second, file);
+  }
+
+}
 
 
 void runQueries(std::ifstream &infile) {
