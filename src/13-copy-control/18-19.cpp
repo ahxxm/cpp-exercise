2dbbd237178746ad5460287ad68619e73a160a6c
ahxxm <i@ahxxm.com>
2016-02-21 23:34:28 +0800

    exercise 19: copy control of 18

--- src/13-copy-control/18-19.cpp
+++ src/13-copy-control/18-19.cpp
@@ -11,6 +11,10 @@ public:
     name = s;
   }
 
+  // 19: copy-control members
+  // new Emploee with same name, but different id
+  Employee(const Employee &): currentID(n++){ };
+
   int id() {
     return currentID;
   }
@@ -25,7 +29,9 @@ int Employee::n = 42;
 TEST(UniqueTest, SomeTest) {
   auto a = Employee("ja");
   auto b = Employee("va");
+  auto c = Employee(b);
   EXPECT_EQ((a.id() == b.id()), false);
+  EXPECT_EQ((a.id() == c.id()), false);
 }
 
 int main(int argc, char *argv[]) {
