d925042a10ce9652961fa53b4aa15ab69a0c014b
ahxxm <i@ahxxm.com>
2016-02-25 22:47:02 +0800

    rvalue reference &&

---
+++ src/13-copy-control/rvalue-reference.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+#include <iostream>
+#include <utility>
+
+
+void rvalue_reference() {
+
+  // variables are lvalues..
+  int k = 42;
+  // int &&r = k; // can't do this
+
+  // after move, we can't make any assumptions
+  // about j
+  int &&j = 2 * 2;
+  int &&jj = std::move(j);
+  std::cout << k << std::endl;
+  std::cout << jj << std::endl;
+}
+
+
+TEST(RValueReferenceTest, SomeTest) {
+  rvalue_reference();
+  EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
