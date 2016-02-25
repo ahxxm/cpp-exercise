7a5ca4967747c8541ee2e76ebb1d9318c6bd5643
ahxxm <i@ahxxm.com>
2016-02-25 11:01:38 +0800

    implementation of StrVec

--- src/13-copy-control/strvec.cpp
+++ src/13-copy-control/strvec.cpp
@@ -2,16 +2,36 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <utility>
 
 
 class StrVec {
 public:
   StrVec(): first(nullptr), first_free(nullptr), cap(nullptr) {};
-  StrVec(const StrVec&);
-  StrVec &operator=(const StrVec&);
-  ~StrVec();
 
-  void push_back(const std::string&);
+  StrVec(const StrVec&s) {
+    auto newdata = alloc_and_copy(s.begin(), s.end());
+    first = newdata.first;
+    first_free = newdata.second;
+    cap = newdata.second;
+  };
+
+  StrVec &operator=(const StrVec&s) {
+    free();
+
+    auto data = alloc_and_copy(s.begin(), s.end());
+    first = data.first;
+    first_free = data.second;
+    return *this;
+  };
+
+  ~StrVec() {free();};
+
+  void push_back(const std::string &s) {
+    check_and_alloc();
+    alloc.construct(first_free++, s);
+  };
+
   std::size_t size() const {
     return first_free - first;
   };
@@ -37,10 +57,42 @@ private:
     }
   }
 
-  std::pair<std::string*, std::string*> alloc_and_copy(const std::string*, const std::string*);
+  // return pointer to begin and one past the last
+  // of newly allocated space
+  std::pair<std::string*, std::string*> alloc_and_copy(const std::string *begin, const std::string *end) {
+    auto data = alloc.allocate(end - begin);
+    return {data, std::uninitialized_copy(begin, end, data)};
+  };
 
-  void free();
-  void reallocate();
+  // destroy elements and deallocate memory space
+  void free() {
+    if (first) {
+      for (auto p = first_free; p != first; ) {
+        alloc.destroy(--p);
+      }
+      alloc.deallocate(first, cap - first);
+    }
+  };
+
+  // allocate, construct, destroy
+  // here std::move frees old memory
+  void reallocate() {
+    auto new_cap = size() ? 2 * size() : 1;
+    auto newdata = alloc.allocate(new_cap);
+
+    auto dest = newdata;
+    auto elem = first;
+    for (size_t i = 0; i != size(); ++i) {
+      alloc.construct(dest++, std::move(*elem++));
+    }
+
+    free();
+
+    first = newdata;
+    first_free = dest;
+    cap = first + new_cap;
+
+  };
 
   std::string *first;
   std::string *first_free;
