5ff8f6091cfacc557b4c0c1ef9aa0baf2d239e87
ahxxm <i@ahxxm.com>
2016-02-28 15:42:19 +0800

    input operator

---
+++ src/14-overloaded-operation-conversion/output-operator.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include <string>
+
+
+class Foo {
+public:
+
+  void operator>>(std::string &i) {
+    i = "naive";
+  };
+};
+
+
+TEST(OutputTest, SomeTest) {
+  std::string j;
+  Foo a;
+  a >> j;
+  EXPECT_EQ(j, "naive");
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
