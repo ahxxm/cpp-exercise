3808b8ff978275bc13d7a419c4b0ce08dcb56c75
ahxxm <i@ahxxm.com>
2016-02-29 22:10:13 +0800

    overload subscriptor

---
+++ src/14-overloaded-operation-conversion/subscript-operator.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include <string>
+
+// must be a member function
+class Foo {
+public:
+  std::string &operator[](std::size_t n) {
+    auto a = new std::string("42");
+    return *a;
+  }
+};
+
+
+TEST(SubscriptTest, SomeTest) {
+  Foo a;
+  EXPECT_EQ(a[1], "42");
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
