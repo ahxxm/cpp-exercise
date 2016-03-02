f15d3e6b45761ca622010f850d19be01ee4a6ba7
ahxxm <i@ahxxm.com>
2016-03-02 13:55:46 +0800

    call signature map

---
+++ src/14-overloaded-operation-conversion/call-signature.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include <map>
+#include <string>
+#include <utility>
+
+int adder(int &i, int &j) {
+  return i + j;
+}
+
+int ( * add)(int &i, int &j) = adder;
+
+TEST(SignatureTest, SomeTest) {
+  std::map<std::string, int( *)(int &, int& ) > binops;
+  auto mapper = std::pair<std::string, int( *)(int &, int&)> {"+", add};
+  binops.insert( {"+", add});
+}
+
+int main(int argc,  char *argv[]) {
+  ::testing::InitGoogleTest( & argc,  argv);
+  int ret =  RUN_ALL_TESTS();
+  return ret;
+}
