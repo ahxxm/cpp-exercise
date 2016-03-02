24e02fb7d387e8e86c8e29eae2be565a0ca232f0
ahxxm <i@ahxxm.com>
2016-03-02 15:51:47 +0800

    std::function

--- src/14-overloaded-operation-conversion/call-signature.cpp
+++ src/14-overloaded-operation-conversion/call-signature.cpp
@@ -2,17 +2,26 @@
 #include <map>
 #include <string>
 #include <utility>
+#include <functional>
 
 int adder(int &i, int &j) {
   return i + j;
 }
 
-int ( * add)(int &i, int &j) = adder;
+int (*add)(int &i, int &j) = adder;
 
 TEST(SignatureTest, SomeTest) {
   std::map<std::string, int(*)(int &, int&)> binops;
   auto mapper = std::pair<std::string, int(*)(int &, int&)> {"+", add};
   binops.insert({"+", add});
+
+  // null function object to store callable objects
+  // with call signature..
+  std::function<int(int &, int &)> f;
+  std::function<int(int &, int &)> f1(nullptr);
+  std::function<int(int, int)> f2 = [](int i, int j) {return i + j;};
+  EXPECT_EQ(f2(1, 5), 6);
+
 }
 
 int main(int argc, char *argv[]) {
