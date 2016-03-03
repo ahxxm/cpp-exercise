7d1e95d4818795ebc01eb8df73d782376536e1aa
ahxxm <i@ahxxm.com>
2016-03-03 10:25:38 +0800

    dynamic bind example

--- src/15-oop/oop.cpp
+++ src/15-oop/oop.cpp
@@ -6,6 +6,10 @@ public:
   virtual int answer() const {
     throw std::exception();
   };
+
+  virtual int bind() {
+    return 1;
+  }
 };
 
 class VV: public V {
@@ -15,14 +19,31 @@ public:
     return 42;
   };
 
+  // also requires virtual function
+  // so to use dynamic bind, a virtual base function is
+  // required...
+  int bind() override {
+    return 2;
+  }
 
   int a;
 };
 
+int dynamic(V &v) {
+  return v.bind();
+}
+
 
 TEST(VirtualFunctionTest, SomeTest) {
   VV a;
+  VV b;
+  V c;
   EXPECT_EQ(a.answer(), 42);
+
+  EXPECT_EQ(dynamic(a), 2);
+  EXPECT_EQ(dynamic(b), 2);
+  EXPECT_EQ(dynamic(c), 1);
+
 }
 
 int main(int argc, char *argv[]) {
