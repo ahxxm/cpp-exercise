243a5066bd2d1ce0c0680fd9c85b4cf52d530661
ahxxm <i@ahxxm.com>
2016-03-14 23:12:10 +0800

    template arg deduction

---
+++ src/16-templates/template-arg-deduction.cpp
@@ -0,0 +1,31 @@
+#include "gtest/gtest.h"
+#include <string>
+
+
+template <typename T>
+T fobj(T a, T) {
+  return a;
+};
+
+template <typename T>
+T fref(const T &a, const T&) {
+  return a;
+};
+
+
+TEST(DeductionTest, SomeTest) {
+  std::string s1("something");
+  const std::string s2("else");
+  fobj(s1, s1); // calls (string,string), ignore const
+  fref(s1, s2); // convert s1 to const
+
+  int a[10], b[42];
+  fobj(a, b); // (int *, int *)
+  // fref(a, b); // error, type not consistent
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
