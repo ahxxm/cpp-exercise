6beea92477c0a16aa2a75a116167c086713e0aaf
ahxxm <i@ahxxm.com>
2016-03-17 17:46:30 +0800

    recursive print template

--- src/16-templates/arg-pack.cpp
+++ src/16-templates/arg-pack.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include <iostream>
+#include <ostream>
 
 
 template <typename T, typename... Args>
@@ -12,8 +13,27 @@ void foo(const T &, const Args&... rest) {
 };
 
 
+template <typename T>
+std::ostream &print(std::ostream &os, const T &t) {
+  return os << t;
+};
+
+template <typename T, typename... Args>
+std::ostream &print(std::ostream &os, const T &t, const Args&... rest) {
+  // first arg
+  os << t << ", ";
+
+  // recursively rest: first of rest is bounded to t, others to rest
+  // ... until match print above
+  return print(os, rest...);
+};
+
+
 TEST(ArgPackTest, SomeTest) {
   foo(1, 2, 3, "jar");
+  print(std::cout, 1, 2, 3, "42");
+  std::cout << std::endl;
+
 }
 
 int main(int argc, char *argv[]) {
