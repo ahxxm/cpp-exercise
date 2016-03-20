02e85cee2b025f0ea67a84b1d766cc0cc31ec173
ahxxm <i@ahxxm.com>
2016-03-20 16:57:27 +0800

    bitset initialization and conversion

--- src/17-specialized-library-facilities/bitset.cpp
+++ src/17-specialized-library-facilities/bitset.cpp
@@ -12,6 +12,19 @@ TEST(BitsetTest, SomeTest) {
   EXPECT_EQ(bitvec[0], 1);
   EXPECT_EQ(bitvec[1], 0);
 
+  // high order bits are discarded
+  std::bitset<13> bitvec1(0xbeef);
+
+  // 2 and 3 are 1, others 0
+  std::bitset<32> bitvec2("1100");
+
+  // conversions
+  std::cout << bitvec1.to_string() << std::endl;
+  std::cout << bitvec2.to_string() << std::endl;
+  std::cout << bitvec2.to_ullong() << std::endl;
+
+
+
 }
 
 int main(int argc, char *argv[]) {
