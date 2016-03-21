e2d78c4d2207f1573efc2f4b876b52ec0f2fb9ae
ahxxm <i@ahxxm.com>
2016-03-21 23:08:27 +0800

    random distribution engine

--- src/17-specialized-library-facilities/random-number.cpp
+++ src/17-specialized-library-facilities/random-number.cpp
@@ -8,7 +8,13 @@ TEST(RandomNumberTest, SomeTest) {
   std::default_random_engine e(42);
   for (std::size_t i = 0;i < 10; ++i) {
     std::cout << e() << std::endl;
-  }
+  };
+
+  // !
+  std::uniform_real_distribution<double> u(0, 1);
+  for (std::size_t i = 0;i < 10; ++i) {
+    std::cout << u(e) << std::endl;
+  };
 }
 
 int main(int argc, char *argv[]) {
