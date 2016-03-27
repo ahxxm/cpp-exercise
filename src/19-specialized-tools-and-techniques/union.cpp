1bb1852bd4ed05246c6208522656d998daaf36c0
ahxxm <i@ahxxm.com>
2016-03-27 16:37:20 +0800

    union introduction

---
+++ src/19-specialized-tools-and-techniques/union.cpp
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+
+
+// union is special kind of class:
+// - might have multiple data members,
+// but only one of them may have a value.
+// - storage needed is its largest data member.
+// - once a value is assigned, others become undefined.
+// - can't have a reference member.
+// - may not inherit or be inherited from,
+// no virtual function.
+
+union Token {
+  char cval;
+  int ival;
+  double dval;
+};
+
+void union_test() {
+  Token first_token = {'a'};
+  Token last_token;
+  Token *pt = new Token;
+
+  last_token.ival = 2;
+  std::cout << first_token.cval << std::endl;
+  pt->dval = 42.0;
+
+};
+
+
+TEST(UnionTest, SomeTest) {
+  union_test();
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
