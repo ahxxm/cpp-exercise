ead2d7025b34a8005a5827e61f2d57fe3c6d63c4
ahxxm <i@ahxxm.com>
2016-03-27 10:53:53 +0800

    multi inheritance constructor

--- src/18-large-problem-tools/multiple-and-virtual-inheritance.cpp
+++ src/18-large-problem-tools/multiple-and-virtual-inheritance.cpp
@@ -3,23 +3,34 @@
 class Foo {
 public:
   int i = 1;
+  Foo() = default;
+  Foo(int);
 
 };
 
 class Bar {
 public:
   int j = 2;
+  Bar() = default;
+  Bar(int);
 
 };
 
 // multiple inheritance
 class A: public Foo, public Bar {
+public:
+  // required by custom constructor
+  A() = default;
 
+  // must define its own Class(int)
+  A(int i):k(i) {};
+private:
+  int k;
 };
 
 
 TEST(InheritanceTest, SomeTest) {
-  A a;
+  A a(1);
   EXPECT_EQ(a.i, 1);
   EXPECT_EQ(a.j, 2);
 
