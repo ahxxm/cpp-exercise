307a805c4b5987fa45d3f837ad65ad3280df3164
ahxxm <i@ahxxm.com>
2016-03-27 14:03:19 +0800

    new and delete operator

--- src/19-specialized-tools-and-techniques/memory-allocation.cpp
+++ src/19-specialized-tools-and-techniques/memory-allocation.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include <string>
+#include <new>
 
 
 void test() {
@@ -12,6 +13,19 @@ void test() {
   delete sp; // destroy pointer and free pointed memory
   delete [] arr; // destroy 10 strings and free memory
 
+};
+
+class Foo {
+  // new and delete operator does not change
+  // new and delete expression
+
+  // called when new a Foo object
+  // must return void*
+  void *operator new(std::size_t, void *) noexcept;
+
+  // called when delete a Foo pointer
+  // must return void
+  void operator delete(void *, std::nothrow_t &) noexcept;
 };
 
 
