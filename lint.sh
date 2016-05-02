259dd0bae06d8d76e74e639f8bf90faf047cdbc3
ahxxm <i@ahxxm.com>
2016-05-02 21:40:04 +0800

    revert ef3e3444069ef1d121fac82bad21afaa5cfa7d2b

--- lint.sh
+++ lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/google/styleguide/raw/gh-pages/cpplint/cpplint.py
-python cpplint.py --extensions=hpp,cpp --output=emacs --recursive --counting=detailed ./
+pip install --user cpplint
+cpplint --extensions=hpp,cpp --output=emacs --recursive --counting=detailed ./
 exit 0
