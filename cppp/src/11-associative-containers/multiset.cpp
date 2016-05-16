7d5554d304c0625fb732734961c7222e13780354
ahxxm <i@ahxxm.com>
2016-05-16 09:27:27 +0800

    fix comparison between signed and unsigned

--- cppp/src/11-associative-containers/multiset.cpp
+++ cppp/src/11-associative-containers/multiset.cpp
@@ -15,9 +15,12 @@ TEST(SetTest, SomeTest) {
   // multiset accepts same value.. in a set?!
   std::set<int> iset(ivec.begin(), ivec.end());
   std::multiset<int> miset(ivec.cbegin(), ivec.cend());
-  EXPECT_EQ(ivec.size(), 20);
-  EXPECT_EQ(iset.size(), 10);
-  EXPECT_EQ(miset.size(), 20);
+  unsigned int vec_size = 20;
+  unsigned int set_size = 10;
+  unsigned int miset_size = vec_size;
+  EXPECT_EQ(ivec.size(), vec_size);
+  EXPECT_EQ(iset.size(), set_size);
+  EXPECT_EQ(miset.size(), miset_size);
 }
 
 int main(int argc, char *argv[]) {
