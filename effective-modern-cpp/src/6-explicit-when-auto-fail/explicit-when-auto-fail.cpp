1a1c6cf0226c3eb71e64f717a067d7c5eb9fadf3
ahxxm <i@ahxxm.com>
2016-05-27 15:48:55 +0800

    auto fails in vector<bool>[]

---
+++ effective-modern-cpp/src/6-explicit-when-auto-fail/explicit-when-auto-fail.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include "gtest/gtest.h"
+
+
+static std::vector<bool> wat = {true, false};
+
+std::vector<bool> features(const int&) {
+  return wat;
+};
+
+
+void fail() {
+  auto w = 1;
+
+  // jar could be a dangling pointer, depends on implementation of std::vector<bool>::reference.
+  // here auto fail because [] returns:
+  // 1 bit per bool in vector<bool>, ref to bool is forbidden in cpp.
+  // auto jar_ptr = features(w)[1];
+  bool jar = features(w)[1];
+  std::cout << &jar << std::endl;
+}
+
+
+TEST(ExplicitTest, SomeTest) {
+  fail();
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
