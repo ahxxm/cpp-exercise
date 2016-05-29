e90b87ee04dbad0d54596832e8732c5a2597b731
ahxxm <i@ahxxm.com>
2016-05-29 10:08:50 +0800

    explicit and implicit override

--- effective-modern-cpp/src/12-declare-override/override.cpp
+++ effective-modern-cpp/src/12-declare-override/override.cpp
@@ -4,8 +4,19 @@
 class Widget {
 public:
   // & work when Widget is lvalue, && when rvalue
-  int doWork() & {return 1;};
-  int doWork() && {return 0;};
+  virtual int doWork() & {return 1;};
+  virtual int doWork() && {return 0;};
+};
+
+
+class Derived: public Widget {
+public:
+  // won't compile without specifier
+  // int doWork() override {return 2;};
+
+  // explicit and implicit override
+  int doWork() & override {return 2;};
+  int doWork() && {return 3;};
 };
 
 
@@ -13,6 +24,10 @@ TEST(OverrideTest, SomeTest) {
   Widget w;
   EXPECT_EQ(w.doWork(), 1);
   EXPECT_EQ(Widget().doWork(), 0);
+
+  Derived ww;
+  EXPECT_EQ(ww.doWork(), 2);
+  EXPECT_EQ(Derived().doWork(), 3);
 }
 
 int main(int argc, char *argv[]) {
