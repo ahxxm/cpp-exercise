b6780b6b4bfc5c20258b43b6e025109de8146a7c
ahxxm <i@ahxxm.com>
2016-06-01 13:49:54 +0800

    add one more fixme for shared_ptr

--- effective-modern-cpp/src/19-shared-ptr/shared-ptr.cpp
+++ effective-modern-cpp/src/19-shared-ptr/shared-ptr.cpp
@@ -16,6 +16,8 @@
 // shared_ptr to same object can have different deleter
 class Widget {};
 
+// FIXME: test performance, overhead of control block,
+// custom deleter and atomic ref count operation
 
 void dif_deleter() {
   auto customDel1 = [](Widget *pw) {delete pw;};
