29dddfd0d21e002aec7a529a613cd5dac7524ddd
ahxxm <i@ahxxm.com>
2016-06-01 08:57:53 +0800

    unique ptr usage

--- effective-modern-cpp/src/18-unique-ptr/unique-ptr.cpp
+++ effective-modern-cpp/src/18-unique-ptr/unique-ptr.cpp
@@ -1,8 +1,35 @@
+#include <memory>
 #include "gtest/gtest.h"
 
 
 // unique_ptr should be first considered
 // almost as fast as raw pointer
+
+class Investment {};
+
+class Stock: public Investment {};
+class Bond: public Investment {};
+class Cash: public Investment {};
+
+
+// custom deleter: more efficient
+auto delInvmt = [](Investment *pInvestment) {
+  // log
+  delete pInvestment;
+};
+
+using return_type = std::unique_ptr<Investment, decltype(delInvmt)>;
+
+template<typename... Ts>
+return_type makeInvestment(Ts&&... params) {
+  return_type pInv(nullptr, delInvmt);
+
+  //
+  // if (stock should be created) {
+  pInv.reset(new Stock(std::forward<Ts>(params)...));
+  // }
+  return pInv;
+};
 
 
 TEST(UniquePtrTest, SomeTest) {
