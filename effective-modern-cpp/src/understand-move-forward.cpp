b4d189926c00bc6517828b1aaf58277fe9758883
ahxxm <i@ahxxm.com>
2016-06-02 14:40:21 +0800

    close-to-STL move()

---
+++ effective-modern-cpp/src/understand-move-forward.cpp
@@ -0,0 +1,24 @@
+#include <type_traits>
+#include <utility>
+#include "gtest/gtest.h"
+
+// std::move casts into rvalue,
+// std::forward sometimes casts into rvalue
+
+// close to STL ipml
+template<typename T>
+typename std::remove_reference<T>::type&& move(T &&param) {
+  using Return_T = typename std::remove_reference<T>::type&&;
+  return static_cast<Return_T>(param);
+};
+
+
+TEST(MoveForwardTest, SomeTest) {
+  EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
