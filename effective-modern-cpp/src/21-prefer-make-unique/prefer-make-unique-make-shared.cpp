7ce28f3e4dd42c04a15ea5283436ce7d69389aa7
ahxxm <i@ahxxm.com>
2016-06-02 12:26:53 +0800

    advantage of make_shared

--- effective-modern-cpp/src/21-prefer-make-unique/prefer-make-unique-make-shared.cpp
+++ effective-modern-cpp/src/21-prefer-make-unique/prefer-make-unique-make-shared.cpp
@@ -2,6 +2,8 @@
 #include <utility>
 #include "gtest/gtest.h"
 
+class Widget {};
+
 // make_unique join in C++14
 // simple one
 
@@ -10,9 +12,18 @@ std::unique_ptr<T> make_unique(Ts&&... params) {
   return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
 };
 
+void auto_make() {
+  // make avoids duplication
+  // exception-safe: leak between new and cast
+  // performace: allocate object+control-block once
+  auto upw1 {make_unique<Widget>()};
+  std::cout << &upw1 << std::endl;
+
+}
+
 
 TEST(MakePtrTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  auto_make();
 }
 
 int main(int argc, char *argv[]) {
