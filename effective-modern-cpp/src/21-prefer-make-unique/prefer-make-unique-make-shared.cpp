0d851f5d265b970297c5d8a5f056662823f790af
ahxxm <i@ahxxm.com>
2016-06-02 11:29:15 +0800

    simple make_unique for cpp11

---
+++ effective-modern-cpp/src/21-prefer-make-unique/prefer-make-unique-make-shared.cpp
@@ -0,0 +1,22 @@
+#include <memory>
+#include <utility>
+#include "gtest/gtest.h"
+
+// make_unique join in C++14
+// simple one
+
+template <typename T, typename... Ts>
+std::unique_ptr<T> make_unique(Ts&&... params) {
+  return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
+};
+
+
+TEST(MakePtrTest, SomeTest) {
+  EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
