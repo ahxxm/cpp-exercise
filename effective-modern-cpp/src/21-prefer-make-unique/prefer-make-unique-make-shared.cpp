8b03a5df1728bb834210bf5467a3fac16dd6c379
ahxxm <i@ahxxm.com>
2016-06-02 12:31:45 +0800

    brace can't be forwarded perfectly

--- effective-modern-cpp/src/21-prefer-make-unique/prefer-make-unique-make-shared.cpp
+++ effective-modern-cpp/src/21-prefer-make-unique/prefer-make-unique-make-shared.cpp
@@ -12,18 +12,22 @@ std::unique_ptr<T> make_unique(Ts&&... params) {
   return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
 };
 
-void auto_make() {
+int auto_make() {
   // make avoids duplication
   // exception-safe: leak between new and cast
   // performace: allocate object+control-block once
   auto upw1 {make_unique<Widget>()};
   std::cout << &upw1 << std::endl;
 
+  // forwards paren, size 10 of value 20
+  // brace can't be forwarded perfectly
+  auto puv = make_unique<std::vector<int>> (10, 20);
+  return static_cast<int>(puv->size());
 }
 
 
 TEST(MakePtrTest, SomeTest) {
-  auto_make();
+  EXPECT_EQ(auto_make(), 10);
 }
 
 int main(int argc, char *argv[]) {
