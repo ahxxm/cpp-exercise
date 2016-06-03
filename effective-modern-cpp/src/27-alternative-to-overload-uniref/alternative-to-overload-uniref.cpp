e539a9915226c807eacb2553917c8e42bce098be
ahxxm <i@ahxxm.com>
2016-06-03 17:50:07 +0800

    use decay+enable_if for type comparison and dispatch

--- effective-modern-cpp/src/27-alternative-to-overload-uniref/alternative-to-overload-uniref.cpp
+++ effective-modern-cpp/src/27-alternative-to-overload-uniref/alternative-to-overload-uniref.cpp
@@ -1,7 +1,25 @@
+#include <type_traits>
 #include "gtest/gtest.h"
 
 
 // tag param by std::is_integral and std::true_type
+
+
+// enable_if<condition>
+class Person {
+public:
+
+  // when T is not person, nor const/volatile person
+  // decay removes these(also turn array into pointer)
+  template<typename T, typename = typename std::enable_if<
+                         !std::is_same<Person,
+                                       typename std::decay<T>::type
+                                       >::value
+                         >::type>
+  explicit Person(T &&) {};
+
+};
+
 
 
 TEST(AvoidOverloadUniversalRefTest, SomeTest) {
