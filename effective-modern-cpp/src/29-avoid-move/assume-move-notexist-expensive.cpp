e75e86bdf57c19dc011195d09aa52d9d1537904d
ahxxm <i@ahxxm.com>
2016-06-04 14:34:15 +0800

    compilers are required to use move

---
+++ effective-modern-cpp/src/29-avoid-move/assume-move-notexist-expensive.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+
+// compilers are **required** to use move
+
+
+
+TEST(NoMoveTest, SomeTest) {
+  EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
