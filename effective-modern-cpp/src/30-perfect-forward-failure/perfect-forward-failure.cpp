666d670972278480efb62ba25fbce55db6cf0590
ahxxm <i@ahxxm.com>
2016-06-04 20:36:42 +0800

    failure when forward static const member

--- effective-modern-cpp/src/30-perfect-forward-failure/perfect-forward-failure.cpp
+++ effective-modern-cpp/src/30-perfect-forward-failure/perfect-forward-failure.cpp
@@ -11,6 +11,8 @@
 
 // Brace initializer
 void f(const std::vector<int> &) {}
+void f(int) {}
+
 
 template<typename... Ts>
 void fwd(Ts&&... params){
@@ -32,10 +34,32 @@ void brace_fail() {
 
 // 0/NULL as nullptr
 
+// static const member
+
+class Widget {
+public:
+  // declaration, not definition.
+  static const int MinVals = 28;
+};
+
+void static_const_fail() {
+  f(Widget::MinVals);
+
+  // link fail: undefined ref to static const member.
+
+  // reference are treated like pointers by compiler, pass MinVals
+  // ref is the same as pass by pointer, so there has to be memory
+  // to be pointed to, thus require it to be defined.
+  // note: not all compiler require it to be defined.
+
+  // fwd(Widget::MinVals);
+}
+
 
 
 TEST(PerfectForwardFailureTest, SomeTest) {
   brace_fail();
+  static_const_fail();
 }
 
 int main(int argc, char *argv[]) {
