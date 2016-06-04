6810a55138add3067a3694a9a5222d36b217e037
ahxxm <i@ahxxm.com>
2016-06-04 14:29:18 +0800

    4 context of ref collapse

--- effective-modern-cpp/src/28-ref-collapse/reference-collapsing.cpp
+++ effective-modern-cpp/src/28-ref-collapse/reference-collapsing.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "gtest/gtest.h"
 
 
@@ -8,8 +9,57 @@ template<typename T>
 void f(T &&param);
 
 
+// ref collapse happen in 4 contexts
+// 1. template instantialtion
+// 2. type generator for auto(same as 1, see Item-2)
+template <typename T>
+void func(T &&) {}
+
+class Widget {};
+Widget widgetFactory() {
+  return Widget();
+}
+
+void template_ins() {
+  Widget w;
+  func(w); // T: Widget&
+  func(widgetFactory()); // T: Widget
+}
+
+void auto_type() {
+  Widget w;
+
+  // w is lvalue ref, w1 is also
+  auto &&w1 = w;
+  std::cout << &w1 << std::endl;
+
+
+  // w2 is rvalue ref
+  auto &&w2 = widgetFactory();
+  std::cout << &w2 << std::endl;
+
+}
+
+
+// 3. typedef/using
+template<typename T>
+class Def {
+public:
+  typedef T&& RR;
+};
+
+void typedef_collapse() {
+  Def<int&> w; // RR is int& &&, int&
+  std::cout << &w << std::endl;
+}
+
+// 4. decltype
+
+
 TEST(RefCollapseTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  template_ins();
+  auto_type();
+  typedef_collapse();
 }
 
 int main(int argc, char *argv[]) {
