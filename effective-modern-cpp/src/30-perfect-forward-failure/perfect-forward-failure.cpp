eb3fe40868feccf9c7bf7ea512239530fc4c563a
ahxxm <i@ahxxm.com>
2016-06-04 20:14:56 +0800

    first fail case of forward: brace initializer

--- effective-modern-cpp/src/30-perfect-forward-failure/perfect-forward-failure.cpp
+++ effective-modern-cpp/src/30-perfect-forward-failure/perfect-forward-failure.cpp
@@ -1,12 +1,32 @@
+#include <vector>
+#include <utility>
 #include "gtest/gtest.h"
 
 // forward: one function pass its param to another
 // goal/perfect: second function receive the same object
 
+// Failures
+template<typename... T>
+void fwd(T&&... params) {
+  f(std::forward<T>(params)...);
+}
+
+
+// Brace initializer
+void f(const std::vector<int> &) {}
+
+void brace_fail() {
+  // direct: converts to std::vector implicitly
+  f({1, 2, 3});
+
+  // forward fail: fwd **deduce** T to be initializer_list
+  // fwd({1, 2, 3})
+}
+
 
 
 TEST(PerfectForwardFailureTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  brace_fail();
 }
 
 int main(int argc, char *argv[]) {
