acbefb0ad417cfba08c90820b5df8cd2b65ddb53
ahxxm <i@ahxxm.com>
2016-06-05 13:57:30 +0800

    init capture's scope

--- effective-modern-cpp/src/32-init-capture/use-init-capture-to-move.cpp
+++ effective-modern-cpp/src/32-init-capture/use-init-capture-to-move.cpp
@@ -1,11 +1,27 @@
+#include <memory>
+#include <utility>
 #include "gtest/gtest.h"
 
 // C++11 does not give a way to **move**
 // object into clojure
 // C++14 does
 
+class Widget {
+
+};
+
+void init_capture() {
+  auto pw = std::make_shared<Widget>();
+
+  // only in C++14
+  // pw: scope in clojure class
+  // std::move(pw): scope as the same of lambda expression
+  // auto func = [pw = std::move(pw)]() {return 42;};
+}
+
+
 TEST(InitCaptureTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  init_capture();
 }
 
 int main(int argc, char *argv[]) {
