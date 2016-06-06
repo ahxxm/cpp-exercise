f862e652cf5c9d2b52a4b7201483e46f59040055
ahxxm <i@ahxxm.com>
2016-06-06 12:34:46 +0800

    check future status

--- effective-modern-cpp/src/36-launch-async/launch-async.cpp
+++ effective-modern-cpp/src/36-launch-async/launch-async.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+#include <future>
 #include "gtest/gtest.h"
 
 // std::async: request function be run according to std::async **launch policy**
@@ -7,8 +9,24 @@
 
 // default policy is mixed, so no way to predict behavior.
 
+int f() {
+  return 42;
+}
+
+
+void check_future() {
+  auto future = std::async(f);
+
+  auto one_s = std::chrono::seconds(1);
+  if (future.wait_for(one_s) == std::future_status::deferred) {
+    // use get() or wait()
+  } else {
+    // wait for timeout
+  }
+}
+
 TEST(LaunchAsyncTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  check_future();
 }
 
 int main(int argc, char *argv[]) {
