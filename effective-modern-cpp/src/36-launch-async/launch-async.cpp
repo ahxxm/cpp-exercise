be1fd91589a59ab118b9e9bcbf735eb8e31bd2f6
ahxxm <i@ahxxm.com>
2016-06-06 12:24:31 +0800

    2 async policy

---
+++ effective-modern-cpp/src/36-launch-async/launch-async.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+// std::async: request function be run according to std::async **launch policy**
+// 2 policies:
+// std::launch::async - must run asynchronously
+// std::launch::deferred - may run when get() or wait() called on future object
+
+TEST(LaunchAsyncTest, SomeTest) {
+  EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
