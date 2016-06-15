c459329ea4eb7bee4df10defa01b6a11f8f1d319
ahxxm <i@ahxxm.com>
2016-06-15 17:51:37 +0800

    1-3 linear search for arbitrary vector

--- clrs/src/2/insert-sort.cpp
+++ clrs/src/2/insert-sort.cpp
@@ -18,12 +18,27 @@ auto insertSort(T elems) {
   return elems;
 }
 
+// 1-3 linear search
+template<typename T>
+auto linearSearch(std::vector<T>vec, const T &target) {
+  for (int i = 0; i < static_cast<int>(vec.size()); ++i) {
+    if (vec[i] == target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 
 TEST(InsertSortTest, SomeTest) {
   std::vector<int> ve {1, 4, 2, 5, 3};
   auto vc = insertSort(ve);
   std::vector<int> ve_result {1, 2, 3, 4, 5};
+
+  std::vector<int> ve_short {1, 4, 2, 5, 3};
   EXPECT_EQ(vc, ve_result);
+  EXPECT_EQ(linearSearch(ve_short, 1), 0);
+  EXPECT_EQ(linearSearch(ve_short, 100), -1);
 }
 
 int main(int argc, char *argv[]) {
