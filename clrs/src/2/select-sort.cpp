25e1d8a3ab84e44fbb9c779ea81c2010af493023
ahxxm <i@ahxxm.com>
2016-06-17 09:52:55 +0800

    select sort

---
+++ clrs/src/2/select-sort.cpp
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+
+using TT = std::vector<int>;
+
+TT selectSort(TT ve) {
+  int min_index, tmp;
+  for (unsigned long i = 0; i < ve.size() - 1; ++i) {
+    min_index = i;
+    for (unsigned long j = i + 1; j < ve.size(); ++j) {
+      // find smallest
+      if (ve[j] < ve[i]) {
+        min_index = j;
+      }
+
+      // exchange with vector[i]
+      if (static_cast<unsigned long>(min_index) != i) {
+        tmp = ve[i];
+        ve[i] = ve[min_index];
+        ve[min_index] = tmp;
+      }
+    }
+
+  }
+  return ve;
+};
+
+
+
+TEST(SelectSortTest, SomeTest) {
+  TT a = {1, 4, 3, 2};
+  TT b = {1, 2, 3, 4};
+  EXPECT_EQ(selectSort(a), b);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
