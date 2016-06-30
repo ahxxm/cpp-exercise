c73cdf83d6ab11a10f257cde46b56af992f16bc3
ahxxm <i@ahxxm.com>
2016-06-30 10:21:19 +0800

    add sort TODO

---
+++ clrs/src/sort/counting-sort.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+// counting sort
+// radix sort
+
+
+TEST(RadixTest, SomeTest) {
+  EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
