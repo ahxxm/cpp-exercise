2dcf41312be75d306ec22bc4d97bd9ead27d9f8b
ahxxm <i@ahxxm.com>
2016-07-26 08:28:05 +0800

    record left/right while insert

--- clrs/src/data-structure/binary-tree.cpp
+++ clrs/src/data-structure/binary-tree.cpp
@@ -12,6 +12,7 @@ struct Node {
   T value;
   Node<T> *left;
   Node<T> *right;
+  Node<T> *parent;
 
   Node(T val): value(val){};
 
@@ -21,32 +22,56 @@ struct Node {
 template <typename T>
 class BinaryTree {
 public:
+  using node_t = Node <T>;
+
   // constructor
   // TODO: build from std::vector<T>, or first/last iterator to be more generic?
 
   // interface
   void insert(T val) {
-    insert(root, val);
+    insert(root, nullptr, val, 0);
   };
 
   int node_count() {return size;}
   bool is_empty() {return size == 0;}
 
+  // FIXME: test
+  node_t *search(T val) {
+    std::cout << root->value << std::endl;
+    auto iter = root;
+    std::cout << iter->value << std::endl;
+    while(iter) {
+      if (iter->value == val) {
+        break;
+      }
+
+      if (iter->value > val) {
+        iter = iter->left;
+      } else {
+        iter = iter->right;
+      }
+    }
+
+    return iter;
+  }
+
+
+  void postorder() {
+    postorder(root, 0);
+  }
+
   // TODO:
   void pre_order(); // pre/in/post order traversal, test
-  void print();
   void balance(); // balance the whole tree
-  // node_t *search(T val) ;
-  // int height(); // impl after balance
+  void print();
+  // int height(); // impl after balance?
   // void rotate_left(); // and right
 
 private:
-  using node_t = Node <T>;
-
-  node_t *root;
   int size = 0;
+  node_t *root;
 
-  void insert(node_t *node, T val) {
+  void insert(node_t *node, node_t *parent, T val, int left = 0) {
     // insert below this parent
     // actual insert
     if (!node) {
@@ -54,14 +79,38 @@ private:
       node->left = nullptr;
       node->right = nullptr;
       size += 1;
+
+      if(!parent) {return;}
+      if (left) {
+        parent->left = node;
+      } else {
+        parent->right = node;
+      }
       return;
     }
 
-    // find nullptr
+    // find position
     if (val < node->value) {
-      insert(node->left, val);
+      insert(node->left, node, val, 1);
     } else {
-      insert(node->right, val);
+      insert(node->right, node, val, 0);
+    }
+  }
+
+  void postorder(node_t *p, int indent = 0)
+  {
+    if(p) {
+      if(p->left) {
+        postorder(p->left, indent + 4);
+      }
+      if(p->right) {
+        postorder(p->right, indent + 4);
+      }
+
+      if(indent) {
+        std::cout << std::setw(indent) << ' ';
+      }
+      std::cout << p->value << "\n ";
     }
   }
 
@@ -76,6 +125,11 @@ TEST(BinarySearchTreeTest, SomeTest) {
   a.insert(5);
   a.insert(4);
   EXPECT_EQ(a.node_count(), 5);
+
+  // a.postorder();
+  // auto b = a.search(5);
+  // EXPECT_EQ(b->value, 5);
+  // EXPECT_EQ(b->left->value, 4);
 }
 
 int main(int argc, char *argv[]) {
