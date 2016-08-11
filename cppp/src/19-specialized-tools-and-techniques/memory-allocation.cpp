d75440b124f3f84c4a8b2cfdeeb7bdb2d222f236
ahxxm <i@ahxxm.com>
2016-08-11 10:50:44 +0800

    prevent raw throw

--- cppp/src/19-specialized-tools-and-techniques/memory-allocation.cpp
+++ cppp/src/19-specialized-tools-and-techniques/memory-allocation.cpp
@@ -26,7 +26,9 @@ class Foo {
     if (void *mem = malloc(size)) {
       return mem;
     } else {
-      throw std::bad_alloc();
+      // "throw will always call terminate()"
+      // throw std::bad_alloc();
+      return nullptr;
     };
   };
 
