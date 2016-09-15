6dde962a1d89a2b6e3de2407f429ff45edac1f93
ahxxm <i@ahxxm.com>
2016-09-15 09:04:24 +0100

    solve projecteuler problem 1

--- euler/1.cpp
+++ euler/1.cpp
@@ -3,10 +3,23 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 // https://projecteuler.net/problem=1
 
-// TODO:
+#define N 999
+// "below"
+
+int sum(int n, int d) {
+  // N and divider
+  auto count = n / d;
+  return count * (d + count * d) / 2;
+}
+
+int euler1() {
+  return sum(N, 3) + sum(N, 5) - sum(N, 15);
+}
 
 TEST(ThreeFiveTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  EXPECT_EQ(sum(10, 3), 18);
+  EXPECT_EQ(sum(14, 3), 30);
+  EXPECT_EQ(euler1(), 233168);
 }
 
 int main(int argc, char *argv[]) {
