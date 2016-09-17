898005251d5e5a687f52ed3de70148cb635f6746
ahxxm <i@ahxxm.com>
2016-09-17 14:17:07 +0800

    project euler problem 6

---
+++ euler/6.cpp
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+
+// (1+2+3...+100)^2-(1^2+2^2+....100^2)
+#define N 100
+
+using ll = long long;
+ll solve() {
+  ll sum_square = 0;
+  ll sum = 0;
+  for(ll i = 1;i <= N; ++i) {
+    sum += i;
+    sum_square += i * i;
+  }
+
+  ll square_sum = sum * sum;
+  return square_sum - sum_square;
+
+}
+
+TEST(SumSquareDiffTest, SomeTest) {
+  EXPECT_EQ(solve(), 25164150);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
