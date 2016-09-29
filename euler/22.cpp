34ab66cf313bf174e6cd1f3de313c4e600963c14
ahxxm <i@ahxxm.com>
2016-09-29 22:44:55 +0800

    problem 22 without slution
    might require cpp-netlib or boost asio to parse remote file

---
+++ euler/22.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include "gtest/gtest.h"
+
+/*
+
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+
+*/
+
+auto download() {
+  std::string url = "https://projecteuler.net/project/resources/p022_names.txt";
+}
+
+
+TEST(NameScoreTest, SomeTest) {
+  EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
