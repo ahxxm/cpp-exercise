3443a25d1011b0557701c2abe7a1f45795016b22
ahxxm <i@ahxxm.com>
2016-12-18 19:22:47 +0800

    constexpr and const member function

--- effective-modern-cpp/src/15-constexpr/constexpr.cpp
+++ effective-modern-cpp/src/15-constexpr/constexpr.cpp
@@ -2,15 +2,25 @@
 
 
 // constexpr is interface of object/function
-// FIXME: re-read this chapter..
-
-
 // constexpr (conceptually) means value is known during compilation
-// FIXME: (technically it's during `translation`)
+// applies to VERY SIMPLE and non-virtual function with very simple return type
+// constexpr implies const
+// http://stackoverflow.com/questions/14116003/difference-between-constexpr-and-const
+
+
+
+// constexpr const int *NP = &N;
+// both required: constexpr for NP, const for N
+
+// for member function like
+// constexpr void f() const;
+// const: A const member function is a member function that guarantees it will not modify the object or call any non-const member functions
+
+constexpr int f() {return 42;}
 
 
 TEST(ConstexprTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  EXPECT_EQ(f(), 42);
 }
 
 int main(int argc, char *argv[]) {
