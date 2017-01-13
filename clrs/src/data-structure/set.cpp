587e65c1f23a78c7182c8137df8c2d9375390a55
ahxxm <i@ahxxm.com>
2017-01-13 13:01:12 +0800

    set: size, find, emplace

--- clrs/src/data-structure/set.cpp
+++ clrs/src/data-structure/set.cpp
@@ -11,14 +11,25 @@ public:
     return ImplTree.getSize() == 0;
   };
 
-  std::size_t size() {
+  auto size() {
     return ImplTree.getSize();
   };
 
+  bool find(int i) {
+    auto node_p = ImplTree.search(i);
+    if (node_p) {return true;}
+    return false;
+  }
+
+  bool emplace(int i) {
+    auto node_p = ImplTree.search(i);
+    if (node_p) {return false;}
+    ImplTree.insert(i);
+    return true;
+  }
+
   // TODO:
-  set(const set &) = delete;
-  set &operator=(std::initializer_list<int>) = delete;
-  // emplace
+  // set &operator=(std::initializer_list<int>) = delete;
   // const_iterator find(const key_type &) const;
   // size_type count(const key_type &) const;
   // size_type count(const key_type &);
@@ -38,7 +49,19 @@ private:
 
 
 TEST(SetTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  auto s = set();
+  EXPECT_EQ(s.size(), 0);
+  s.emplace(1);
+  EXPECT_EQ(s.size(), 1);
+  EXPECT_EQ(s.find(1), true);
+  EXPECT_EQ(s.find(2), false);
+
+  s.emplace(1);
+  EXPECT_EQ(s.size(), 1);
+  EXPECT_EQ(s.find(1), true);
+  s.emplace(2);
+  EXPECT_EQ(s.size(), 2);
+  EXPECT_EQ(s.find(2), true);
 }
 
 int main(int argc, char *argv[]) {
