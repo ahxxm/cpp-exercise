49659c4764e0113b0963ef85c750c06992cdfaad
ahxxm <i@ahxxm.com>
2017-01-20 12:25:11 +0800

    quicksort

--- clrs/src/sort/quick-sort.cpp
+++ clrs/src/sort/quick-sort.cpp
@@ -1,12 +1,44 @@
+#include <algorithm>
 #include "gtest/gtest.h"
 
-// TODO: quicksort
-// partition(A, p, r) returns 2 index q and t, so that:
-// p <= q <=t <= r, A[q..t] are equal
 
+int partition(int *arr, const int left, const int right) {
+  const int mid = left + (right - left) / 2;
+  const int pivot = arr[mid];
+  std::swap(arr[mid],arr[left]);
+
+  auto i = left + 1;
+  auto j = right;
+  while (i <= j) {
+    while(i <= j && arr[i] <= pivot) {
+      ++i;
+    }
+
+    while(i <= j && arr[j] > pivot) {
+      --j;
+    }
+
+    if (i < j) {
+      std::swap(arr[i], arr[j]);
+    }
+  }
+  std::swap(arr[i - 1],arr[left]);
+  return i - 1;
+}
+
+void quicksort(int *arr, const int left, const int right){
+  if (left >= right) {
+    return;
+  }
+  int part = partition(arr, left, right);
+  quicksort(arr, left, part - 1);
+  quicksort(arr, part + 1, right);
+}
 
 TEST(QuickSortTest, SomeTest) {
-  EXPECT_EQ(1, 1);
+  int arr[8] = {1, 3, 2, 4, 999, 6};
+  int sz = sizeof(arr)/sizeof(arr[0]);
+  quicksort(arr, 0, sz - 1);
 }
 
 int main(int argc, char *argv[]) {
