77a9b490cf23989dee56bad5baabb8e4f7d4a675
ahxxm <i@ahxxm.com>
2020-09-23 20:26:28 +0800

    boost 1.74.0

--- install-boost.sh
+++ install-boost.sh
@@ -3,7 +3,7 @@ set -ex
 
 if [ ! -d "${HOME}/${CCOMPILER}-boost_${BOOST_VERSION}/include" ]; then
     # hardcoded download url because filename
-    wget -O boost_${BOOST_VERSION}.tar.bz2 https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.bz2
+    wget -O boost_${BOOST_VERSION}.tar.bz2 https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.bz2
     tar jxf boost_${BOOST_VERSION}.tar.bz2
     cd boost_${BOOST_VERSION}
     ./bootstrap.sh --with-toolset=$TOOLSET --prefix=${HOME}/${CCOMPILER}-boost_${BOOST_VERSION}
